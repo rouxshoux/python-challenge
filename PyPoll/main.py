642a08e7dc8c3ecf1cf943cc1081c468568480b6
joshua robles <rouxshoux@gmail.com>
2021-04-08 04:35:55 -0500

    some note

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+import os
+import csv
+path = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
+total_months = 0
+total_profit_loss = 0
+prev_profit_loss = 0
+month_change = 0
+average_month_change = 0
+greatest_increase = 0
+greatest_increse_month = ""
+greatest_decrease = 0
+greatest_increase_month = ""
