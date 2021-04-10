efc0f67b02f5b18ea88801ae59c2de6bb7231321
joshua robles <rouxshoux@gmail.com>
2021-04-10 04:36:23 -0500

    some note

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,50 +1,52 @@
 import os
 import csv
-total_months = 0
-total_profit_loss = 0
-prev_profit_loss = 0
-month_change = 0
-average_month_change = 0
-greatest_increase = 0
-greatest_increse_month = ""
-greatest_decrease = 0
-greatest_increase_month = ""
 
-path = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
-with open(path, newline="") as csvfile:
-    reader = csv.reader(csvfile, delmiter=",")
-    header = next(csv.reader)
-    row = next(reader)
-    previous_row = int(row[1])
-    total_months += 1
-    net_amount += int(row[1])
-    greatest_increase = int(row[1])
-    greatest_increaase_month = row[0]
-    
-    for row in reader:
-        total_months += 1
-        net_amount += int(row[1])
-        revenue_change = int(row[1]) - previous_row
-        monthly_change.append(revenue_change)
-        previous_row = int(row[1])
-        month_count.apend(row[0])
-        if int(row[1]) > greatest_increase:
-            greatest_increase = int(row[1])
-            greatest_increase_month = row[0]
-            
-        if int(row[1]) < greatest_decrease:
-            greatest_decrease_month = row[0]
-            
-    average_change = sum(monthly_change) / len(monthly_change)
-    highest = max(monthly_change)
-    lowest = min(monthly_change)
-    
-print(f"Financial Anlysis")
-print(f"--------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${net_amount}")
-print(f"Average Change: ${average_change:.2f}")
-print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
-print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
+pybank_path = "Desktop/coding/01_All_My_Repos/python-challenge/PyBank/Resources/budget_data.csv"
 
-      
+ttl_month = 0
+month_change = []
+net_change = []
+great_inc = ["", 0]
+great_dec = ["", 999999999999999]
+ttl_net = 0
+
+with open(pybank_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    first_row = next(csvreader)
+    ttl_month = ttl_month + 1
+    ttl_net = ttl_net + int(first_row[1])
+    previous_net = int(first_row[1])
+    
+    for row in csvreader:
+        ttl_month = ttl_month + 1
+        ttl_net - ttl_net + int(first_row[1])
+        net_change = int(row[1]) - previous_net
+        previous_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_change = month_change + [row[0]]
+        if net_change > great_inc[1]:
+            great_inc[0] = row[0]
+            great_ince[1] = net_change
+        if net_change < great_dec[1]:
+            great_dec[0] = row[0]
+            great_dec[1] = net_change
+            
+net_month_average = sum(net_change_list)/len(net_change_list)
+print("Financial Analysis")
+print(f"Total Months: {ttl_month}")
+print(f"Total: ${ttl_net}")
+print(f"Average Change: ${net_month_average}")
+print(f"Greatest Increase in Profits: {great_inc[0]} (${great_inc[1]})")
+print(f"Greatest Decrease in Profits: {great_dec[0]} (${great_dec[1]})")
+
+output = (
+   f"\nFinancial Analysis\n"
+   f"----------------------------\n"
+   f"Total Months: {ttl_month}\n"
+   f"Total: ${ttl_net}\n"
+   f"Average  Change: ${net_month_average}\n"
+   f"Greatest Increase in Profits: {great_inc[0]} (${great_inc[1]})\n"
+   f"Greatest Decrease in Profits: {great_dec[0]} (${great_dec[1]})\n")
+with open("output.txt", 'w') as txt_file:
+    txt_file.write(output)
